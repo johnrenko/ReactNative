2e85cc98144f08720d783d63e233996a6e1433c1
johnrenko <vishnou@gmail.com>
2015-11-08 15:49:42 +1100

    added registration

---
+++ App/Components/RegisterPage.js
@@ -0,0 +1,112 @@
+var React = require('react-native');
+
+var {
+	View,
+	Text,
+	TextInput,
+	TouchableWithoutFeedback,
+	TouchableHighlight,
+	StyleSheet,
+} = React;
+
+var RegisterPage = React.createClass({
+
+  getInitialState: function() {
+    return {
+      login: "",
+      pw: "",
+    };
+  },
+  
+  _handleBackButtonPress: function() {
+    this.props.navigator.pop();
+  },
+
+  render: function() {
+    return (
+      <View style={styles.pageContainer}>
+        <View style={styles.containerRight}>
+          <Text style={styles.label}>
+            Email
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(login) => this.setState({login})}
+            value={this.state.text} />
+          <Text style={styles.label}>
+            Password
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(pw) => this.setState({pw})}
+            value={this.state.pw} />
+        </View>
+        <TouchableHighlight style={styles.button} onPress={this._onPressButton} underlayColor={'#d3d3d3'}>
+        <Text style={styles.buttonText}>
+            Register
+        </Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  pageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+    padding: 20,
+    paddingBottom: 0
+  },
+  containerRight: {
+    alignItems: 'flex-start',
+  },
+  containerLeft: {
+    alignItems: 'flex-end',
+  },
+  button: {
+    backgroundColor: '#f5f5f5',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#d3d3d3',
+    padding: 16,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  buttonLink: {
+  	textAlign: 'center',
+    fontSize: 16,
+    paddingTop: 20,
+    color: 'blue',
+    textDecorationLine: 'underline'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: '#d3d3d3',
+    borderWidth: 1,
+    marginTop: 3,
+    marginBottom: 10,
+    padding: 10,
+    borderRadius:4,
+    backgroundColor: '#fff'
+  },
+  label: {
+    fontSize:12,
+    textAlign: 'left',
+  },
+});
+
+module.exports = RegisterPage;
--- index.ios.js
+++ index.ios.js
@@ -6,10 +6,14 @@
 
 var React = require('react-native');
 var Parse = require('parse').Parse;
+var LoginPage = require('./App/Components/LoginPage');
+var RegisterPage = require('./App/Components/RegisterPage');
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   TextInput,
   TouchableHighlight,
   View,
@@ -20,101 +24,28 @@ Parse.initialize(
   'vJKvQk3pnhfjMU3JyIzoPrV2G00JPm5Fe8cPHiUe'
 );
 
-var reactNative = React.createClass({
 
-  getInitialState: function() {
-    return {
-      login: "",
-      pw: "",
-    };
-  },
+
+var reactNative = React.createClass({
 
   render: function() {
     return (
-      <View style={styles.pageContainer}>
-        <View style={styles.containerRight}>
-          <Text style={styles.label}>
-            Email
-          </Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={(login) => this.setState({login})}
-            value={this.state.text}
-          />
-          <Text style={styles.label}>
-            Password
-          </Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={(pw) => this.setState({pw})}
-            value={this.state.pw}
-          />
-        </View>
-        <TouchableHighlight style={styles.button} onPress={this._onPressButton} underlayColor={'#d3d3d3'}>
-        <Text style={styles.buttonText}>
-            Login
-        </Text>
-        </TouchableHighlight>
-      </View>
+      <NavigatorIOS
+        style = {styles.container}
+        initialRoute={{
+          title: 'Login Page',
+          component: LoginPage
+        }} />
+       
     );
   }
 });
 
 var styles = StyleSheet.create({
-  pageContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#F5FCFF',
-    padding: 20,
-  },
-  containerRight: {
-    alignItems: 'flex-start',
-  },
-  containerLeft: {
-    alignItems: 'flex-end',
-  },
-  button: {
-    backgroundColor: '#f5f5f5',
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: '#d3d3d3',
-    padding: 16,
-  },
-  buttonText: {
-    textAlign: 'center',
-    fontSize: 16,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  input: {
-    height: 40,
-    borderColor: '#d3d3d3',
-    borderWidth: 1,
-    marginTop: 3,
-    marginBottom: 10,
-    padding: 10,
-    borderRadius:4,
-    backgroundColor: '#fff'
-  },
-  label: {
-    fontSize:12,
-    textAlign: 'left',
-  },
-  labelSwitch: {
-    fontSize:12,
-    flex: 1,
-  },
-  switch: {
-    flex: 1,
-  }
+    container: {
+        flex: 1
+    }
 });
+
 
 AppRegistry.registerComponent('reactNative', () => reactNative);
